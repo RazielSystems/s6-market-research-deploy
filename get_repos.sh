8bc6461b7281441f34ffd2dcb294a611307e1203
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2025-01-07 17:45:44 -0600

    get repos

---
+++ get_repos.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:PDNMX/database-test.git database
+git clone git@github.com:RazielSystems/s6-market-research-back.git back
+git clone git@github.com:RazielSystems/s6-market-research-front.git front
